df92491dba98e95c7885371c8a7a7260ab2f8762
MehrabChowdhury <93445792+FaysalMehrab@users.noreply.github.com>
2026-02-15 09:06:38 +0600

    Update Average Time of Process per Machine.sql

--- Average Time of Process per Machine.sql
+++ Average Time of Process per Machine.sql
@@ -4,3 +4,18 @@ JOIN Activity end
 ON start.machine_id = end.machine_id AND start.process_id = end.process_id
 AND start.activity_type='start' and end.activity_type='end'
 GROUP BY machine_id;
+
+
+
+--OR--------------------------------------------------------------
+
+SELECT
+a.machine_id,
+ROUND(
+AVG(CASE WHEN a.activity_type = 'end' THEN a.timestamp END) -
+AVG(CASE WHEN a.activity_type = 'start' THEN a.timestamp END), 3
+) AS processing_time
+FROM
+activity a
+GROUP BY
+a.machine_id;
